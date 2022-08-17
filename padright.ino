56b970ad57e894b40ffa38cef6c43831c1b9ceb0
oggysaud245 <rajusaud245@gmail.com>
2022-08-18 02:26:15 +0545

    shifted to onebutton

--- padright.ino
+++ padright.ino
@@ -1,14 +1,28 @@
 #include "padrack.h"
-padrack rack1,rack2,rack3,rack4,rack5;
-byte selectButton, okButton, backButton;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "OneButton.h"
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+padrack rack1, rack2, rack3, rack4, rack5;
+OneButton selectButton(A2, true,true);
+OneButton okButton(A2, true,true);
+OneButton backButton(A2, true,true);
+
+byte arrow[8] = {
+    0b00000,
+    0b11100,
+    0b10010,
+    0b01001,
+    0b01001,
+    0b10010,
+    0b11100,
+    0b00000};
 void setup()
 {
     Serial.begin(9600);
-    pinMode(selectButton, INPUT_PULLUP);
-    pinMode(okButton, INPUT_PULLUP);
-    pinMode(backButton, INPUT_PULLUP);
+
 }
 void loop()
 {
-
 }
