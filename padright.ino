d5e99825a41e0f8b55307f515ca668368cdaca38
oggysaud245 <rajusaud245@gmail.com>
2022-08-18 02:09:09 +0545

    button setup

--- padright.ino
+++ padright.ino
@@ -1,7 +1,14 @@
-void setup(){
-
-
+#include "padrack.h"
+padrack rack1,rack2,rack3,rack4,rack5;
+byte selectButton, okButton, backButton;
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(selectButton, INPUT_PULLUP);
+    pinMode(okButton, INPUT_PULLUP);
+    pinMode(backButton, INPUT_PULLUP);
 }
-void loop(){
-    
+void loop()
+{
+
 }
